60868b0a0a87eda25ece6d58a7e263cb155b50a0
Anton S <66568776+Arizoba@users.noreply.github.com>
2024-01-19 20:37:41 -0500

    File struc

---
+++ TeamZindo_GJ/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,131 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class PlayerMovement : MonoBehaviour
+{
+    
+    [Header("Movement")]
+
+    public float moveSpeed;
+
+    public float groundDrag;
+
+    public float jumpforce;
+    public float jumpCooldown;
+    public float airMultiplier;
+    bool readyToJump = true;
+
+    [Header("Keybinds")]
+    public KeyCode jumpkey = KeyCode.Space;
+
+    [Header("Ground Check")]
+    public float playerHeight;
+    public LayerMask whatIsGround;
+    bool grounded;
+
+    public Transform orientation;
+
+    float horizontalInput;
+    float verticalInput;
+
+    Vector3 moveDirection;
+
+    Rigidbody rb;
+    public TextMeshProUGUI screentext;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>(); 
+        rb.freezeRotation = true;
+        screentext.text = "";
+    }
+
+    private void MyInput()
+    {
+        horizontalInput = Input.GetAxisRaw("Horizontal");
+        verticalInput = Input.GetAxisRaw("Vertical");
+
+        if(Input.GetKey(jumpkey) && readyToJump && grounded) 
+        {
+            readyToJump = false;
+            Jump();
+            Invoke(nameof(ResetJump), jumpCooldown);
+        }
+
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        grounded = Physics.Raycast(transform.position, Vector3.down, playerHeight * 0.5f + 0.2f, whatIsGround); // check if standing on something with ground layer
+        MyInput();
+        SpeedControl();
+
+        if (grounded) // air control
+            rb.drag = groundDrag;
+        else
+            rb.drag = 0;
+
+        if (rb.transform.position.y <= -2) // if fall into canyon
+        {
+            screentext.text = "Fail \n Press \"r\" to quick restart";
+            rb.gameObject.SetActive(false);
+        }
+
+    }
+
+    private void FixedUpdate()
+    {
+        MovePlayer();
+
+    }
+
+    private void MovePlayer()
+    {
+        moveDirection = orientation.forward * verticalInput + orientation.right * horizontalInput;  
+
+        rb.AddForce(moveDirection.normalized * moveSpeed * 10f, ForceMode.Force); 
+
+        if (grounded) rb.AddForce(moveDirection.normalized * moveSpeed * 10f, ForceMode.Force);
+
+        else if (!grounded)
+        {
+            rb.AddForce(moveDirection.normalized * moveSpeed * 10f * airMultiplier, ForceMode.Force);
+        }
+    }
+
+    private void SpeedControl() // set max player velocity
+    {
+        Vector3 flatVel = new Vector3(rb.velocity.x, 0f, rb.velocity.z);
+
+        if(flatVel.magnitude > moveSpeed)
+        {
+            Vector3 limitedVel = flatVel.normalized* moveSpeed;
+            rb.velocity = new Vector3(limitedVel.x, rb.velocity.y, limitedVel.z);
+        }
+    }
+    private void Jump()
+    {
+        rb.velocity = new Vector3(rb.velocity.x, 0f, rb.velocity.z);
+
+        rb.AddForce(transform.up * jumpforce, ForceMode.Impulse);
+    }
+
+    private void ResetJump()
+    {
+        readyToJump = true;
+    }
+
+    private void OnTriggerEnter(Collider other) // When enter trophy display win then stop the game from being played
+    {
+        if (other.gameObject.CompareTag("Trophy"))
+        {
+            other.gameObject.transform.parent.gameObject.SetActive(false);
+            screentext.text = "Win \n press \"r\" to quick reset";
+            rb.gameObject.SetActive(false);
+        }
+    }
+}
