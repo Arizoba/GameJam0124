cb1a89440b1844cd0b72d9f3ec6168f4b5245e7a
Anton S <66568776+Arizoba@users.noreply.github.com>
2024-01-21 12:36:35 -0500

    fail

--- TeamZindo_GJ/Assets/Scripts/PTSD.cs
+++ TeamZindo_GJ/Assets/Scripts/PTSD.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 
 public class PTSD : MonoBehaviour
 {
+
+    public Sprite[] images;
+    public float imageinterval = 10;
+
     // Start is called before the first frame update
     void Start()
     {
