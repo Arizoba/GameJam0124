f6e128d9b3d7040186ae1b9e916dd3aeed49f2d4
Anton S <66568776+Arizoba@users.noreply.github.com>
2024-01-21 10:14:04 -0500

    added spider spawn

--- TeamZindo_GJ/Assets/Scripts/EM/EM_Jumping.cs
+++ TeamZindo_GJ/Assets/Scripts/EM/EM_Jumping.cs
@@ -11,19 +11,22 @@ public class EM_Jumping : MonoBehaviour
     float speed_y=0;
 
 
+    private float starty;
+
     Vector3 startpos;
     void Start()
     {
+        starty = transform.position.y;
     }
-
+    
     // Update is called once per frame
     void Update()
     {
-        if (transform.position.y <= 2.5) // ground or below
+        if (transform.position.y <= starty-2) // ground or below
         {
             // need to jump
             force_y = 0;
-            speed_y = 6;
+            speed_y = 3;
         }
         else // in air
         {
